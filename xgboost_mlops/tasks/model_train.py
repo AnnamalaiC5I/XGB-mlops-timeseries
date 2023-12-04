60ffaecbf63c827d1c68af3c2ead51f675b9aaf2
Annamalai <Annamalai@course5i.com>
2023-12-04 15:09:09 +0530

    sql query removed+2

--- xgboost_mlops/tasks/model_train.py
+++ xgboost_mlops/tasks/model_train.py
@@ -151,7 +151,7 @@ class ModelTrain(Task):
 
                 dbutils = DBUtils(spark)
 
-                inference_data_df = fs.read_table('default.xgbtimeseries')
+                inference_data_df = fs.read_table('default.xgbtimeseries2')
 
                 train_X, train_y, test_X, test_y, train_df, test_df, training_set = self.load_data(inference_data_df)
 
